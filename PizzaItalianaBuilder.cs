06479dbb82bc2f7d848b8bf3c72224d16e90ff79
Jose Merlo <josemielmer2002@hotmail.com>
2023-01-11 23:43:20 -0500

    Agregar archivos de proyecto.

---
+++ PizzaItalianaBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoBuilder
+{
+    public class PizzaItalianaBuilder : PizzaBuilder
+    {
+        public PizzaItalianaBuilder()
+        {
+            _descripcion = "Pizza Italiana";
+        }
+        public override Agregado BuildAgregado()
+        {
+            return new Anchoas();
+        }
+
+        public override Masa BuildMasa()
+        {
+            return new ALaPiedra();
+        }
+
+        public override Salsa BuildSalsa()
+        {
+            return new Oliva();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoBuilder;
+
+PizzaBuilder builder = new PizzaItalianaBuilder();
+Pizza pizza1 = builder.BuildPizza();
+
+PizzaBuilder builder2 = new PizzaLightBuilder();
+Pizza pizza2 = builder2.BuildPizza();
+
+PizzaBuilder builder3 = new PizzaMuzzaBuilder();
+Pizza pizza3 = builder3.BuildPizza();
+
+
+Console.WriteLine(pizza1.ToString());
+Console.WriteLine(pizza2.ToString());
+Console.WriteLine(pizza3.ToString());
+
+
